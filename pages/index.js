8783fc0cfc845a0ef0b5cf089a9232eead8d6624
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2022-06-29 22:50:15 +0900

    url changed

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
-import { supabase } from "../utils/supabase";
-import { useUser } from "../context/user";
-
 export default function Home() {
-  const { user } = useUser();
-
   return (
     <div>
       <p>Working yo dude!</p>
